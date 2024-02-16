47eadf4b3755d839c5feae33ca12559d87e512b5
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-16 10:17:31 +0100

    clearup prints and restructure file

--- advent/01/main.py
+++ advent/01/main.py
@@ -1,8 +1,5 @@
-import os
-
 def main ():
-    print(os.getcwd())
-    with open("./advent/01A/input") as input :
+    with open("./advent/01/input") as input :
         calibration_value = 0
         for line in input:
             calibration_value += extract_number(line)
@@ -19,8 +16,6 @@ def extract_number(line):
                 last = char
             else :
                 first = char
-    print("f: {}, l:{}".format(first, last))
-
     return join_digits(first, last)
 
 def join_digits(first, last):
@@ -28,8 +23,6 @@ def join_digits(first, last):
         number = first + last
     else:
         number = first + first
-
-    print(number)
     return int(number)
 
 
