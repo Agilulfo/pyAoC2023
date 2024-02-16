a586fb36cbe6a10bc2d26e6057e09413a9e8ae21
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-16 10:24:15 +0100

    prepare shared code

--- advent/01/main.py
+++ advent/01/main.py
@@ -1,30 +1,11 @@
+import shared
+
 def main ():
     with open("./advent/01/input") as input :
         calibration_value = 0
         for line in input:
-            calibration_value += extract_number(line)
+            calibration_value += shared.extract_number(line)
         print("the calibration_value is {0}".format(calibration_value))
-
-
-def extract_number(line):
-    first = None
-    last  = None
-    print(line)
-    for char in line:
-        if str.isdigit(char):
-            if first :
-                last = char
-            else :
-                first = char
-    return join_digits(first, last)
-
-def join_digits(first, last):
-    if last:
-        number = first + last
-    else:
-        number = first + first
-    return int(number)
-
 
 if __name__ == "__main__" :
     main()
---
+++ advent/01/shared.py
@@ -0,0 +1,17 @@
+def extract_number(line):
+    first = None
+    last  = None
+    for char in line:
+        if str.isdigit(char):
+            if first :
+                last = char
+            else :
+                first = char
+    return join_digits(first, last)
+
+def join_digits(first, last):
+    if last:
+        number = first + last
+    else:
+        number = first + first
+    return int(number)
