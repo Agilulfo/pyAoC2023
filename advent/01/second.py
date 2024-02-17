1d1faa48a1b85794aea94fbaa96c293f7fadb531
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-17 10:35:55 +0100

    move map outside of function

--- advent/01/second.py
+++ advent/01/second.py
@@ -1,26 +1,25 @@
 import shared
+word_map = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
 
 def main ():
     with open("./advent/01/input") as input :
         calibration_value = 0
         for line in input:
-
             calibration_value += shared.extract_number(replace_number_words(line))
         print("the calibration_value is {0}".format(calibration_value))
 
 def replace_number_words(line):
     print(line)
-    word_map = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
 
     for k, v in word_map.items():
         line = line.replace(k, v)
