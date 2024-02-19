23f60378200265ba093d87a8fd0496d6f18db888
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-19 11:13:11 +0100

    implement adjacent_submap function and test it

--- advent/day03/first.py
+++ advent/day03/first.py
@@ -38,3 +38,19 @@ def extract_numbers(map):
                     buffer = None
 
     return numbers
+
+def adjacent_submap(full_map, row, start, end):
+    max_row = len(full_map) - 1
+    max_column = len(full_map[0]) - 1
+
+    submap = []
+
+    start_row = max( 0 , row - 1)
+    start_column = max(0 , start - 1)
+    end_row = min ( max_row, row + 1)
+    end_column = min( end + 1, max_column )
+
+    for row_index in range(start_row, end_row + 1):
+        submap.append(full_map[row_index][start_column:end_column + 1])
+
+    return submap
---
+++ advent/day03/test_submap.py
@@ -0,0 +1,53 @@
+from advent.day03 import first
+
+def test_adjacent_submap():
+    full_map = [
+        "...........",
+        "...........",
+        "...YYYY....",
+        "...YXXY....",
+        "...YYYY....",
+        "...........",
+    ]
+
+    submap = first.adjacent_submap(full_map, 3, 4, 5)
+
+    assert submap == [
+        "YYYY",
+        "YXXY",
+        "YYYY",
+    ]
+
+def test_adjacent_submap_corner_top_left():
+    full_map = [
+        "XXY........",
+        "YYY........",
+        "...........",
+        "...........",
+        "...........",
+        "...........",
+    ]
+
+    submap = first.adjacent_submap(full_map, 0, 0, 1)
+
+    assert submap == [
+        "XXY",
+        "YYY"
+    ]
+
+def test_adjacent_submap_corner_bottom_right():
+    full_map = [
+        "...........",
+        "...........",
+        "...........",
+        "...........",
+        "........YYY",
+        "........YXX",
+    ]
+
+    submap = first.adjacent_submap(full_map, 5, 9, 10)
+
+    assert submap == [
+        "YYY",
+        "YXX"
+    ]
