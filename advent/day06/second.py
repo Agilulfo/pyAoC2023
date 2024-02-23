56911d79202bc99061c2cbd7667e61be0c3f9d78
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-23 19:33:40 +0100

    Day 6 part 2 ..

---
+++ advent/day06/second.py
@@ -0,0 +1,12 @@
+from advent.day06 import first
+
+def main():
+    time, distance = first.parse_input()
+    time = fix_kerning(time)
+    distance = fix_kerning(distance)
+
+    # well.. it works, it takes a couple of seconds to run but it runs
+    print(f"Race margin is {first.race_margin((time, distance))}")
+
+def fix_kerning(numbers):
+    return int("".join([str(number) for number in numbers]))
