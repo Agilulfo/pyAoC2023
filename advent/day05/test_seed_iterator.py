c0b8f8d533756f48b0b3d075d686c7fa7656f661
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-24 09:23:02 +0100

    remove forgottne test

--- advent/day05/test_seed_iterator.py
+++
@@ -1,7 +0,0 @@
-from advent.day05 import second
-
-
-def test_genrator():
-    seed_iterator = second.SeedIterator([2, 2, 8, 3])
-    seeds = [seed for seed in seed_iterator]
-    assert seeds == [2, 3, 8, 9, 10]
