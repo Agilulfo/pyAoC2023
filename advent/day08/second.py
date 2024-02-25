ab209d60fd3a53110b97c8729113bd47ab863225
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-25 20:34:48 +0100

    solution Day 8 part 2

--- advent/day08/second.py
+++ advent/day08/second.py
@@ -1,22 +1,34 @@
+import math
+import functools
 from advent.day08 import parsing
 from advent.day08.graph import Graph
 
 
 def main():
+    # my initial strategy (without too much hope) was a bruteforce approach
+    # however as expected things do not allign quickly
+    # since there is some periodicity involved I've figured out that
+    # maybe a Least commond multiple could help to calculate when things
+    # will allign. it worked!
+
     directions, nodes = parsing.parse_input()
     node_ids = [id for id, _, _ in nodes if id[-1] == "A"]
+    periods = []
 
     graph = Graph(nodes)
-    counter = 0
 
-    while not all_z(node_ids):
-        counter += 1
-        node_ids = [graph.follow_path(node_id, directions) for node_id in node_ids]
-    print(f"needed {counter*len(directions)} steps")
+    for node_id in node_ids:
+        counter = 0
+        while node_id[-1] != "Z":
+            counter += 1
+            node_id = graph.follow_path(node_id, directions)
+        periods.append(counter)
+
+    steps = functools.reduce(lcm, periods) * len(directions)
+
+    print(f"needed {steps} steps")
 
 
-def all_z(node_ids):
-    for id in node_ids:
-        if id[-1] != "Z":
-            return False
-    return True
+# thanks google
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
