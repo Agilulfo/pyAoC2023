d4913e5c27fe153a396f5844fa609cf2898a7d50
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-26 09:16:13 +0100

    write test

--- advent/day09/test_prediction.py
+++ advent/day09/test_prediction.py
@@ -4,3 +4,8 @@ from advent.day09.prediction import predict_value
 def test_prediction():
     history = [0, 3, 6, 9, 12, 15]
     assert predict_value(history) == 18
+
+
+def test_prediction_backward():
+    history = [10, 13, 16, 21, 30, 45]
+    assert predict_value(history, forward=False) == 5
