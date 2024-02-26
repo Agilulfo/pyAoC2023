a9172a62c085ed2e438576aa1c95c796037d2070
Flavio Di Natale <dinataleflavio@gmail.com>
2024-02-26 09:19:15 +0100

    Solution day 9 part 2

---
+++ advent/day09/second.py
@@ -0,0 +1,9 @@
+from advent.day09.parsing import parse_input
+from advent.day09.prediction import predict_value
+
+
+def main():
+    oasis_report = parse_input()
+    predictions = [predict_value(history, forward = False) for history in oasis_report]
+    result = sum(predictions)
+    print(f"The sum of all predictions is: {result}")
