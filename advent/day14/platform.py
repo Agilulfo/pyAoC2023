491f5d5e8c59678469e207446e558de0a98100eb
Flavio Di Natale <dinataleflavio@gmail.com>
2024-03-11 12:18:57 +0100

    attempt strategy with static rows and columns

--- advent/day14/platform.py
+++ advent/day14/platform.py
@@ -7,7 +7,8 @@ NUMBER_OF_CYCLES = 1000000000
 
 class Platform:
     def __init__(self, platform):
-        self.stones = []
+        self.rounded_stones = []
+        self.cubic_stones = []
         for row_index, row in enumerate(platform):
             for columnd_index, shape in enumerate(row):
                 if shape != ".":
@@ -17,14 +18,25 @@ class Platform:
                         PLATFORM_SIZE - columnd_index,
                         self,
                     )
-                    self.stones.append(stone)
+                    if stone.shape == "#":
+                        self.cubic_stones.append(stone)
+                    else:
+                        self.rounded_stones.append(stone)
+        self.columns = self._build_columns()
+        self.rows = self._build_rows()
 
     def tilt(self, direction):
         match direction:
             case "N":
-                for column in self._build_columns():
-                    limit = PLATFORM_SIZE + 1
-                    for stone in reversed(column):
+                self.column_mode = True
+                groups = defaultdict(list)
+                for stone in self.rounded_stones:
+                    column = self.columns[stone.column_index - 1]
+                    stopper = column[bisect(column, stone)]
+                    groups[stopper].append(stone)
+                for stopper, stones in groups.items():
+                    limit = stopper.row_index
+                    for stone in stones:
                         stone.roll("N", limit)
                         limit = stone.row_index
             case "W":
@@ -34,9 +46,15 @@ class Platform:
                         stone.roll("W", limit)
                         limit = stone.column_index
             case "S":
-                for column in self._build_columns():
-                    limit = -1
-                    for stone in column:
+                self.column_mode = True
+                groups = defaultdict(list)
+                for stone in self.rounded_stones:
+                    column = self.columns[stone.column_index - 1]
+                    stopper = column[bisect(column, stone) - 1]
+                    groups[stopper].append(stone)
+                for stopper, stones in groups.items():
+                    limit = stopper.row_index
+                    for stone in stones:
                         stone.roll("S", limit)
                         limit = stone.row_index
             case "E":
@@ -56,27 +74,29 @@ class Platform:
             self.spin_cycle()
 
     def get_weight(self):
-        return sum([stone.get_weight() for stone in self.stones])
-
-    # CONTINUE: probably building the columns and rows
-    # each time from scratch is too requiring
-    # maybe I can keep stones sorted in a more efficient way
+        return sum([stone.get_weight() for stone in self.rounded_stones])
 
     def _build_columns(self):
         self.column_mode = True
-        columns = defaultdict(list)
-        for stone in self.stones:
-            column = columns[stone.column_index]
+        columns = [[] for _ in range(PLATFORM_SIZE)]
+        for stone in self.cubic_stones:
+            column = columns[stone.column_index - 1]
             column.insert(bisect(column, stone), stone)
-        return columns.values()
+        for index, column in enumerate(columns):
+            column.append( Stone("#", PLATFORM_SIZE + 1, index + 1, self))
+            column.insert(0, Stone("#", -1, index + 1, self))
+        return columns
 
     def _build_rows(self):
-        self.column_mode = False
-        rows = defaultdict(list)
-        for stone in self.stones:
-            row = rows[stone.row_index]
+        self.column_mode = True
+        rows = [[] for _ in range(PLATFORM_SIZE)]
+        for stone in self.cubic_stones:
+            row = rows[stone.row_index - 1]
             row.insert(bisect(row, stone), stone)
-        return rows.values()
+        for index, row in enumerate(rows):
+            row.append(Stone("#", index + 1, -1, self))
+            row.insert(0, Stone("#", index + 1, PLATFORM_SIZE + 1, self))
+        return rows
 
 
 class Stone:
@@ -85,6 +105,9 @@ class Stone:
         self.row_index = row_index
         self.column_index = column_index
         self.platform = platform
+
+    def __hash__(self):
+        return hash((self.row_index, self.column_index))
 
     def __repr__(self):
         return f"{self.shape}, {self.row_index} {self.column_index}"
