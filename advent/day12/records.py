f5ca08fe21d00d2b9b507d35a400688a44f41848
Flavio Di Natale <dinataleflavio@gmail.com>
2024-03-14 17:14:23 +0100

    messy solution to day 12 part 1

--- advent/day12/records.py
+++ advent/day12/records.py
@@ -15,7 +15,7 @@ class ConditionRecord:
             case 0:
                 return 1
             case _:
-                raise NotImplementedError
+                return count_arrangements(self.damaged_record, self.verification_format)
 
     def reduce(self):
         """Simplify current ConditionRecord
@@ -133,3 +133,113 @@ def find_first_dot(sentence, last_len):
 def shortest_sequence(verification_format):
     minimum_spaces = len(verification_format) - 1
     return sum(verification_format) + minimum_spaces
+
+
+def count_fitss(record, broken_sequence_len):
+    contigious_counter = 0
+    fits_counter = 0
+    first_broken = None
+    gap_point = False
+
+    for index, status in enumerate(record):
+        match status:
+            case ".":
+                contigious_counter = 0
+                if first_broken is not None:
+                    gap_point = True
+            case "#":
+                if first_broken is not None:
+                    if gap_point or index - first_broken + 1 > broken_sequence_len:
+                        return 0
+                else:
+                    first_broken = index
+                contigious_counter += 1
+            case "?":
+                contigious_counter += 1
+        if contigious_counter >= broken_sequence_len and (
+            first_broken is None or index - first_broken + 1 > broken_sequence_len
+        ):
+            fits_counter += 1
+    return fits_counter
+
+
+# ??###?? 3
+
+
+def count_fits(record, broken_sequence_len):
+    first_broken = None
+    last_broken = None
+    gap_point = False
+
+    for index, status in enumerate(record):
+        match status:
+            case ".":
+                if first_broken is not None:
+                    gap_point = True
+            case "#":
+                if first_broken is not None:
+                    last_broken = index
+                    if (
+                        gap_point
+                        or last_broken - first_broken + 1 > broken_sequence_len
+                    ):
+                        return 0
+
+                else:
+                    first_broken = index
+                    last_broken = index
+
+    search_area = None
+    if first_broken is not None:
+        search_area = record[
+            max(0, last_broken - broken_sequence_len + 1) : first_broken
+            + broken_sequence_len
+        ]
+    else:
+        search_area = record
+
+    contigious_counter = 0
+    fits_counter = 0
+
+    for index, status in enumerate(search_area):
+        if status != ".":
+            contigious_counter += 1
+        else:
+            contigious_counter = 0
+
+        if contigious_counter >= broken_sequence_len:
+            fits_counter += 1
+
+    return fits_counter
+
+
+# TODO: memoize
+
+
+def count_arrangements(record, groups):
+    if len(groups) == 1:
+        return count_fits(record, groups[0])
+
+    first_group = groups[:1]
+    remaining_groups = groups[1:]
+    start = min_len(first_group)
+    end = min_len(remaining_groups)
+
+    break_point_combinations = []
+    for break_point in range(start, len(record) - end):
+        if record[break_point] != "#" and fits_in(record[:break_point], first_group[0]):
+            break_point_combinations.append(
+                count_arrangements(record[break_point + 1 :], remaining_groups)
+            )
+
+    return sum(break_point_combinations)
+
+
+def fits_in(record, seq_len):
+    left = record[:-seq_len]
+    right = record[-seq_len:]
+    return "#" not in left and "." not in right
+
+
+def min_len(groups):
+    return sum(groups) + len(groups) - 1
--- advent/day12/test_records.py
+++ advent/day12/test_records.py
@@ -1,5 +1,5 @@
 import pytest
-from advent.day12.records import ConditionRecord
+from advent.day12.records import ConditionRecord, count_fits, count_arrangements
 
 
 def test_count_arrangements_full_match():
@@ -96,3 +96,31 @@ def test_remove_dots():
     r = ConditionRecord("....?????.....", [1])
     assert r.remove_dots()
     assert r.damaged_record == "?????"
+
+
+def test_count_fits():
+    assert count_fits("??###??", 3) == 1
+    assert count_fits("??#?#??", 3) == 1
+    assert count_fits("??##.#??", 3) == 0
+    assert count_fits("??##?#??", 3) == 0
+    assert count_fits("??.?.??", 1) == 5
+    assert count_fits("??.?.??", 2) == 2
+
+
+def test_count_fit():
+    assert count_fits("#?#?#?", 6) == 1
+
+
+def test_count_arrangements():
+    assert count_arrangements("##.##", [2, 2]) == 1
+    assert count_arrangements("?#?.???", [2, 2]) == 4
+    assert count_arrangements("????", [1, 1]) == 3
+    assert count_arrangements("???.###", [1, 1, 3]) == 1
+    assert count_arrangements(".??..??...?##.", [1, 1, 3]) == 4
+    assert count_arrangements("????.#...#...", [4, 1, 1]) == 1
+    assert count_arrangements("????.######..#####.", [1, 6, 5]) == 4
+    assert count_arrangements("?###????????", [3, 2, 1]) == 10
+
+
+def test_count_ar():
+    assert count_arrangements("?#?#?#?#?#?#?#?", [1, 3, 1, 6]) == 1
