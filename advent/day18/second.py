0932549d7797271ddb86fb2bc89eab06a423d679
Flavio Di Natale <dinataleflavio@gmail.com>
2024-06-20 13:53:09 +0200

    partial implementation of area coutning function

--- advent/day18/second.py
+++ advent/day18/second.py
@@ -29,17 +29,22 @@ def decode(instruction):
 class Lake:
     def __init__(self, instructions):
         distance, direction = instructions[0]
-        cursor = Edge((0, 0), distance, direction)
+        cursor = Edge((0, 0), distance, direction, self)
         self.edge_count = 0
         for instruction in instructions[1:]:
             self.edge_count += 1
             next_position = cursor.end()
             distance, direction = instruction
-            cursor.insert_after(Edge(next_position, distance, direction))
+            cursor.insert_after(Edge(next_position, distance, direction, self))
             cursor = cursor.next
 
         self.cursor = cursor.next
         self._identify_direction()
+
+    def removing_edge(self, edge):
+        if self.cursor == edge:
+            self.cursor = edge.next
+        self.edge_count -= 1
 
     def _identify_direction(self):
         tmp_cursor = self.cursor
@@ -55,7 +60,53 @@ class Lake:
         self.cw = cw_counter > 0
 
     def calculate_area(self):
-        return 10
+        area = 0
+
+        while self.edge_count > 4:
+            edge = self.pick_chop_edge()
+
+            print(edge.prev.prev)
+            print(edge.prev)
+            print(edge)
+            print(edge.next)
+            print(edge.next.next)
+
+            chop_distance = min(edge.prev.distance, edge.next.distance)
+            chop_direction = opposite_direction(edge.prev.direction)
+
+            area += (edge.distance + 1) * chop_distance
+            edge.prev.shift(chop_direction, chop_distance)
+            edge.shift(chop_direction, chop_distance)
+            edge.next.shift(chop_direction, chop_distance)
+
+            # TODO: adjust edgest that now might have the same direction
+
+            print(edge.prev.prev)
+            print(edge.prev)
+            print(edge)
+            print(edge.next)
+            print(edge.next.next)
+
+            raise RuntimeError("yo")
+
+        return area + (self.cursor.distance + 1) * (self.cursor.next.distance + 1)
+
+    def pick_chop_edge(self):
+        tmp_cursor = self.cursor
+
+        candidate = None
+
+        first_loop = True
+        while tmp_cursor != self.cursor or first_loop:
+            if tmp_cursor.is_buldge(self.cw):
+                if candidate is None or tmp_cursor < candidate:
+                    candidate = tmp_cursor
+            tmp_cursor = tmp_cursor.next
+            first_loop = False
+
+        return candidate
+
+
 def translate_point(position, direction, distance):
     x, y = position
     match direction:
@@ -93,12 +144,13 @@ def opposite_direction(direction):
 
 
 class Edge:
-    def __init__(self, position, distance, direction):
+    def __init__(self, position, distance, direction, lake):
         self.x, self.y = position
         self.distance = distance
         self.direction = direction
         self.next = self
         self.prev = self
+        self.lake = lake
 
     def insert_after(self, other_edge):
         former_next = self.next
@@ -109,6 +161,7 @@ class Edge:
         former_next.prev = other_edge
 
     def remove(self):
+        self.lake.removing_edge(self)
         self.prev.next = self.next
         self.next.prev = self.prev
 
@@ -117,6 +170,17 @@ class Edge:
 
     def end(self):
         return translate_point(self.start(), self.direction, self.distance)
+
+    def shift(self, direction, distance):
+        if direction == self.direction:
+            self.x, self.y = translate_point(self.start(), direction, distance)
+            self.distance = self.distance - distance
+        elif direction == opposite_direction(self.direction):
+            self.distance = self.distance - distance
+        else:
+            self.x, self.y = translate_point(self.start(), direction, distance)
+        if self.distance == 0:
+            self.remove()
 
     def cw_segment(self):
         sequence = (self.direction, self.next.direction)
@@ -145,4 +209,5 @@ class Edge:
         return self.distance < other.distance
 
     def __repr__(self):
-        return f"Edge: {self.x}, {self.y} - {self.direction} {self.distance}"
+        end_x, end_y = self.end()
+        return f"Edge: {self.x}, {self.y} - {self.direction} {self.distance} - {end_x}, {end_y}"
